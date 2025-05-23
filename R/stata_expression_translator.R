c63aebc25953a7d6ab500929e07d579c377b389a
skranz <sebkranz@gmail.com>
2025-05-23 22:13:50 +0000

    post 2025-05-23 22:13:50

--- R/stata_expression_translator.R
+++ R/stata_expression_translator.R
@@ -27,9 +27,9 @@ translate_stata_expression_to_r = function(stata_expr, context = list(is_by_grou
   r_expr = stringi::stri_replace_all_regex(r_expr, "(\\w+)\\[_n\\]", "$1")
 
   # Handle _n and _N.
-  # Use collapse functions as requested where sensible.
-  r_expr = stringi::stri_replace_all_regex(r_expr, "\\b_n\\b", "collapse::fseq()")
-  r_expr = stringi::stri_replace_all_regex(r_expr, "\\b_N\\b", "collapse::fnobs()")
+  # Use dplyr functions for group-wise context.
+  r_expr = stringi::stri_replace_all_regex(r_expr, "\\b_n\\b", "dplyr::row_number()")
+  r_expr = stringi::stri_replace_all_regex(r_expr, "\\b_N\\b", "dplyr::n()")
 
   # Step 2: Iteratively translate Stata functions (e.g., cond(), round(), log(), etc.)
   # This loop handles nested function calls by repeatedly applying transformations.
